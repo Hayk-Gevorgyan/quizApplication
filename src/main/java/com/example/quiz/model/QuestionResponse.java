1e0e254313f1a36a33d92ce85901b875e9f23e0d
Hayk Gevorgian <g.hayk.111@gmail.com>
2024-07-12 13:03:05 +0400

    QuestionResponse.java created

---
+++ src/main/java/com/example/quiz/model/QuestionResponse.java
@@ -0,0 +1,13 @@
+package com.example.quiz.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuestionResponse {
+    Integer id;
+    String answer;
+}
