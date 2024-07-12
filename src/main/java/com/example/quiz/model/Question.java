095c8812e045b331e18cf17b0b37329d0dbf432d
Hayk Gevorgian <g.hayk.111@gmail.com>
2024-07-12 13:01:22 +0400

    Question.java updated

--- src/main/java/com/example/quiz/model/Question.java
+++ src/main/java/com/example/quiz/model/Question.java
@@ -17,8 +17,6 @@ public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String category;
-    private String difficulty;
     private String text;
     private String option1;
     private String option2;
