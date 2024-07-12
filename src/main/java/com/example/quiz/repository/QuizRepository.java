57f441a7f35af53b4df138ef2202864e911325b0
Hayk Gevorgian <g.hayk.111@gmail.com>
2024-07-12 13:02:06 +0400

    QuizRepository.java updated

--- src/main/java/com/example/quiz/repository/QuizRepository.java
+++ src/main/java/com/example/quiz/repository/QuizRepository.java
@@ -2,8 +2,16 @@ package com.example.quiz.repository;
 
 import com.example.quiz.model.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface QuizRepository extends JpaRepository<Quiz, Integer> {
+    @Query(nativeQuery = true, value = "SELECT q.title FROM Quiz q WHERE q.id = :quizId")
+    String getQuizTitleById(@Param("quizId") Integer quizId);
+
+    Optional<Quiz> findByTitle(String title);
 }
